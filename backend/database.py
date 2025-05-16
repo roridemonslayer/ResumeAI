45b431b25b3853b5756e31f2543158552f9d9ddb
roridemonslay <olaniyideborah63@gmail.com>
2025-05-16 15:14:28 -0400

    db stuff addede

--- backend/database.py
+++ backend/database.py
@@ -6,4 +6,8 @@ def init(app):
   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///resume_ai.db'
   app.config["SQLALCEHMY_TRACK_MODIFICATIONS"] = False #this basically like means it wont track uncceaarty things
 
-  
+  db.init_app(app)
+
+  with app.app_context():
+    db.create_all()
+  return db

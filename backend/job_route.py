af517517f52f4c4ed906adce963c69d2325d9f5f
roridemonslay <olaniyideborah63@gmail.com>
2025-05-22 23:22:33 -0400

    new job_route

--- backend/job_route.py
+++ backend/job_route.py
@@ -2,10 +2,10 @@ from flask import Blueprint, request, jsonify
 
 job_blueprint = Blueprint('job', __name__)
 
-@job_blueprint.route("api/job/submitDes", methods = ['POST'])
+@job_blueprint.route("/api/job/submitDes", methods = ['POST'])
 def submit_description():
     return jsonify({'message': 'Job description submitted successfully'})
 
-@job_blueprint.route("api/job/saveDes", methods = ['POST'])
+@job_blueprint.route("/api/job/saveDes", methods = ['POST'])
 def save_description():
     return jsonify({'message': 'Job description saved successfully'})

0917af8e2a51ba1788067c7810aa2b67a41e9804
roridemonslay <olaniyideborah63@gmail.com>
2025-05-26 23:24:49 -0400

    starting resume parser

--- backend/resume_parser.py
+++ backend/resume_parser.py
@@ -1,2 +1,6 @@
 from pyresparser import ResumeParser
-data = ResumeParser("/path/user/shopify/"). get_extracted_data() # this gets the file path and extracts the date
+
+data = ResumeParser("resume.pdf").get_extracted_data() # this gets the file path and extracts the date
+
+
+print(data)

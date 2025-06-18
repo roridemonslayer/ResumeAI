7f6e70779154ddc195b579519098fed7eae26e17
roridemonslay <olaniyideborah63@gmail.com>
2025-06-17 23:29:32 -0400

    parser

--- backend/resume_paser.py
+++ backend/resume_paser.py
@@ -1,2 +1,7 @@
-from pyresparser import ResumeParser
+from pyresparser import ResumeParser #this is just importing resume parser library
 
+#paser instance 
+
+parser = ResumeParser("resume.pdf") #this is just the file path to my resume
+data = parser.get_extracted_data() #this is getting the extracted data from the pdf 
+print(data)

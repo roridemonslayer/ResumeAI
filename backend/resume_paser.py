90cabe051621e069b29912e0b3dfceb47f911d1d
roridemonslay <olaniyideborah63@gmail.com>
2025-08-08 15:04:42 -0400

    updated resume_parser with parsed_data added

--- backend/resume_paser.py
+++ backend/resume_paser.py
@@ -22,10 +22,10 @@ class ResumeParser: #this makes a class called resume parser
             print("Spacy doesn't work BOOOO") #this prints that like spacey doenst work         
             self.nlp = None #sets nlp to none so the rest of the program knows its missing
     
-    def parse_resume(self, file_path: str, user_id: int = None ) -> Dict:        
+    def parse_resume(self, file_path: str, user_id: int = None ) -> Dict: #returns stuff in a dict/hashmap type with a key being assinged to a value the key being the resume        
         #self just refers to the resume instace, file_path is the path to the resume, the user_id is just for user id and if there isn't one it'll say none, and -> dict returns dict with all of resume paseerd info               
         if not Path(file_path).exists():            
-            raise FileNotFoundError(f"Resume wasn't found: {file_path}") #this js checks if         
+            raise FileNotFoundError(f"Resume wasn't found: {file_path}") #this js checks if the resume was in there      
         if not self.nlp:  # FIXED: removed () because nlp is not a function            
             raise Exception("Spacy couldn't be loaded. Resume not parsed")        
         
@@ -35,7 +35,16 @@ class ResumeParser: #this makes a class called resume parser
         elif file_extension in ['.docx','.doc']:             
             text = self.extract_the_text_from_docx(file_path) #processing method for docx         
         else:             
-            raise ValueError(f"Unsupported file format: {file_path}") # returns false statement         
+            raise ValueError(f"Unsupported file format: {file_path}") # returns false statement that the file type isn't suppsorted like if it isnt a pdf or a docx it wont procress. 
+            
         
-        if not text.strip():            
-            raise ValueError("no text could be found")
+        if not text.strip():   #this is basically saying if no white space is found. this is just checking if the string is empty      
+            raise ValueError("no text could be found")  #print that no text could be found
+        #this is important when Scanned PDFs without OCR (image-only PDFs) corrupted files that opened but contained no text and files with only images/graphics
+
+
+        #now we're going to parse the extracted text
+        parsed_data = self._parse_text(text) #so this line is to set up the parse data like the users name, experience, educaiton etc etc. 
+        #it returnse the parse info in a dict form or "hash map form"
+
+        #now we make the meta data. meta data is

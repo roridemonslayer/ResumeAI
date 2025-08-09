1fb4da360a91e91c9dcef11457495335e0fcb294
roridemonslay <olaniyideborah63@gmail.com>
2025-08-09 14:29:20 -0400

    set up metadata

--- backend/resume_paser.py
+++ backend/resume_paser.py
@@ -53,8 +53,12 @@ class ResumeParser: #this makes a class called resume parser
         parsed_data['metadata'] = { #what this does is create. a metadata fucntion in the dict parrsated data. 
             'file_path':file_path, #this is just the origial resume file tied to the metadata
             'file_name' :Path(file_path).name, #what this does is convert a file like Path("/uploads/resumes/john_doe.pdf").name to john_doe.pdf/ it just outpiut the file name in a simple way.
-            'file_size' : os.get.getsize(file_path), #so what this does it it outputs the data of the resume in byte size so like if a resume has 1000 likes it'll be like file_size = "1000" like that 
-            #this is imporant cuz it allows us to know like how much space each file is taking up and if it's cause slowdown traffic 
+            'file_size' : os.get.getsize(file_path), #this is how much space the file takes up on the disk. the disk of the computer ex. 202020 would be like 2.0 gb
             "parsed_at" : datetime.utcnow().isoformat(), #what this does is just display the time in which the resume was parsed
+            #isoformay converts into "2024-01-15T14:30:25.123456" this format when displaying the time and day it was created. 
+            'user_id' : user_id, #this just tracks the users id. each user is assigned an id 
+            "text_length" :len(text), #this just tracks like the lenghts of the text in the file. for example if a file has like 210010 text include puncation, letters spaces etc 
+            "word_count" :len(text.split())  #this just only tracks the word count not including yk white space, just the words
 
         }
+        return parsed_data
